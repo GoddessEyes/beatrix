90d80a1285068b7c6f41bef9dc1905c448f84a94
goddesseyes <goddesseyes@protonmail.com>
2023-03-21 22:07:25 +0300

    feat: add parse lib

--- lib/beatrix_web/services/parse_service.ex
+++ lib/beatrix_web/services/parse_service.ex
@@ -2,17 +2,22 @@ defmodule Beatrix.ParseService do
   @awesome_elixir_readme_url "https://raw.githubusercontent.com/h4cc/awesome-elixir/master/README.md"
 
   def process_awesome_list do
-    awesome_list = make_request_to_awesome_list()
-    parse_markdown(awesome_list)
+    make_request_to_awesome_list() |> parse_markdown()
   end
 
   defp make_request_to_awesome_list do
-    HTTPoison.get!(@awesome_elixir_readme_url)
+    case HTTPoison.get(@awesome_elixir_readme_url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        body
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts "Not found :("
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
   end
 
-  defp parse_markdown(markdown) do
-    {:ok, ast, []} = EarmarkParser.as_ast(markdown.body)
-    as = 1
-  end
+  defp parse_markdown(response_body) do
+    parsed_md = Md.parse(response_body)
 
+  end
 end
