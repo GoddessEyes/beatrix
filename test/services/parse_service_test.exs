8a8c440aee950b1811021719dd4497daa483e7ac
goddesseyes <goddesseyes@protonmail.com>
2023-03-31 01:40:02 +0300

    feat: apply `mix format`
    * remove unneeded test

--- test/services/parse_service_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule BeatrixWeb.PageControllerTest do
-  use BeatrixWeb.ConnCase
-
-  test "proccess_awesome_list servicer logic", %{}} do
-    Beatrix.ParseService.proccess_awesome_list
-  end
-end
