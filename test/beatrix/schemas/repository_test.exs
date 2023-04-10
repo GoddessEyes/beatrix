698efa09228652b310b64033e7e2e4a8f2f3aa3c
goddesseyes <goddesseyes@protonmail.com>
2023-04-11 00:35:38 +0300

    feat: add CI

--- test/beatrix/schemas/repository_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule Beatrix.Schemas.RepositoryTest do
-  use Beatrix.DataCase
-  alias Beatrix.GithubParser.AwesomeParser
-  alias Beatrix.Repo
-  alias Beatrix.Schemas.Category
-  alias Beatrix.Schemas.Repository
-end
